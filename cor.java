055f174ba4ef0628d66b45ea389e94526044ed25
MrEricL <eli6@stuy.edu>
2018-04-30 11:01:04 -0400

    made the cor better

--- cor.java
+++ cor.java
@@ -4,19 +4,47 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
-
+import java.io.*;
+import java.util.*;
 import javax.swing.*;
 
 public class cor {
-    
-  public static void main(String[] args) throws InterruptedException{
+
+  public static void printCor(){
       int i=0;
-    while(true && i<5){
+      while(true && i<5){
       //Thread.sleep(100);
-	i+=1;
-      System.out.println("(" + MouseInfo.getPointerInfo().getLocation().x + 
-              ", " + 
-              MouseInfo.getPointerInfo().getLocation().y + ")");
+        i+=1;
+        System.out.println("(" + MouseInfo.getPointerInfo().getLocation().x + 
+                          ", " + 
+                          MouseInfo.getPointerInfo().getLocation().y + ")");
     }
+  }
+
+  public static void main(String[] args) throws InterruptedException{
+    while (true){
+      System.out.println("Enter 1 to get cor at current position, 0 to quit\n");
+        try {
+          //Parse Terminal
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          int i = Integer.parseInt(br.readLine());
+          //Check for input
+          if (i == 1){
+                  int x=0;
+                  while(true && x<5){
+                  //Thread.sleep(100);
+                    x+=1;
+                    System.out.println("(" + MouseInfo.getPointerInfo().getLocation().x + 
+                                      ", " + 
+                                      MouseInfo.getPointerInfo().getLocation().y + ")");
+                }
+          }
+          else{
+            break;
+          }
+        }
+        catch ( IOException e ) { }
+    }
+
   }
 }
